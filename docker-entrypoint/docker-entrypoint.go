6e729fdd1f71a84046a3917693ead60fbe8e034d
pprokop <piotr.prokop@intel.com>
2016-05-17 16:57:28 +0200

    Adding rendering of ip address

--- docker-entrypoint/docker-entrypoint.go
+++ docker-entrypoint/docker-entrypoint.go
@@ -3,12 +3,15 @@ package main
 import (
 	"io"
 	"log"
+	"net"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
+	"text/template"
 
 	//For testing purposes
-	// restclient "k8s.io/kubernetes/pkg/client/restclient"
+	restclient "k8s.io/kubernetes/pkg/client/restclient"
 	client "k8s.io/kubernetes/pkg/client/unversioned"
 )
 
@@ -18,7 +21,9 @@ const (
 )
 
 var (
-	Info  *log.Logger
+	//"Info logger""
+	Info *log.Logger
+	//"Error logger"
 	Error *log.Logger
 )
 
@@ -41,14 +46,21 @@ func ExecuteCommandFromAnnotation(command []string) error {
 }
 
 //This function retrives a command section and dependencies section from k8s annotations
-func GetAnnotations(annotations map[string]string) (command []string, deps []string) {
-	command = strings.Split(annotations["command"], ",")
-	deps = strings.Split(annotations["dependencies"], ",")
-	if len(deps) == 0 {
-		return command, nil
+func GetAnnotations(annotations map[string]string) (command []string, serviceDeps []string, configs []string) {
+	command = strings.Split(annotations["command"], " ")
+	if len(command) == 0 || command[0] == "" {
+		Error.Println("Command not specified")
+		os.Exit(1)
 	}
-
-	return command, deps
+	serviceDeps = strings.Split(annotations["service_dependencies"], ",")
+	if len(serviceDeps) == 0 || serviceDeps[0] == "" {
+		serviceDeps = nil
+	}
+	configs = strings.Split(annotations["configs"], ",")
+	if len(configs) == 0 || configs[0] == "" {
+		configs = nil
+	}
+	return command, serviceDeps, configs
 }
 
 //This function check if a service in given namespace exists
@@ -56,7 +68,7 @@ func CheckIfServiceExists(c *client.Client, namespace string, service string) {
 
 	_, err := c.Services(namespace).Get(service)
 	if err != nil {
-		Error.Println(service, "service doesn't exist in", namespace, "namespace.")
+		Error.Println(service, "service doesn't exist in", namespace, "namespace. Error:", err)
 		os.Exit(1)
 	}
 }
@@ -66,7 +78,7 @@ func CheckEndpointsAvailabilty(c *client.Client, namespace string, service strin
 
 	e, err := c.Endpoints(namespace).Get(service)
 	if err != nil {
-		Error.Println(service, "service doesn't exist in", namespace, "namespace.")
+		Error.Println(service, "service doesn't exist in", namespace, "namespace. Error: ", err)
 		os.Exit(1)
 	}
 	if len(e.Subsets) == 0 {
@@ -75,14 +87,95 @@ func CheckEndpointsAvailabilty(c *client.Client, namespace string, service strin
 	return true
 }
 
+//"GetIpFromInterface return always first ip from interface"
+func GetIpFromInterface(iface string) string {
+
+	i, err := net.InterfaceByName(iface)
+	if err != nil {
+		Error.Println(iface, "interface doesn't exist. Error: ", err)
+		os.Exit(1)
+	}
+	addr, err := i.Addrs()
+	if err != nil || len(addr) == 0 {
+		Error.Println(iface, " interface doesn't have ip set. Error: ", err)
+		os.Exit(1)
+	}
+	return strings.Split(addr[0].String(), "/")[0]
+}
+
+func RenderConfigWithIP(config string) {
+
+	t := template.Must(template.New(filepath.Base(config)).ParseFiles(config))
+	file, err := os.OpenFile(config, os.O_RDWR, os.ModeCharDevice)
+	if err != nil {
+		Error.Println(err)
+	}
+	params := make(map[string]string)
+	params["IP"] = GetIpFromInterface(EnvExists("INTERFACE_NAME", os.Getenv("INTERFACE_NAME")))
+	err = t.Execute(file, params)
+	if err != nil {
+		Error.Println(err)
+	}
+
+}
+func EnvExists(name string, env string) string {
+	if env == "" {
+		Error.Println("Environment variable ", name, " is empty")
+		os.Exit(1)
+	}
+	return env
+}
+
+func WaitForServiceDependency(c *client.Client, namespace string, deps []string) {
+
+	seviceDepState := WAITING
+	if deps == nil {
+		seviceDepState = READY
+		Info.Println("Container has no service dependencies")
+	}
+
+	for seviceDepState == WAITING {
+
+		for i := range deps {
+
+			service := strings.TrimSpace(deps[i])
+			CheckIfServiceExists(c, namespace, service)
+
+			if !CheckEndpointsAvailabilty(c, namespace, service) {
+				Info.Println(service, " service has no endpoints avaiable -> State waiting")
+				seviceDepState = WAITING
+				break
+			}
+
+			Info.Println(service, " service has at least one running endpoint. -> State ready")
+			seviceDepState = READY
+
+		}
+		Info.Println("All dependencies resolved")
+	}
+
+}
+
+func RenderConfigs(configs []string) {
+	if configs == nil {
+		Info.Println("Container has no configs to render")
+	}
+	for i := range configs {
+		RenderConfigWithIP(configs[i])
+		Info.Println("Rendering config: ", configs[i])
+	}
+}
+
 func InitLogger(linfo io.Writer, lerror io.Writer) {
 	Info = log.New(linfo, "Entrypoint INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
 	Error = log.New(lerror, "Entrypoint Error: ", log.Ldate|log.Ltime|log.Lshortfile)
 }
+
 func main() {
 	//Those envs should be set as DownwardAPI http://kubernetes.io/docs/user-guide/downward-api/
-	podName := os.Getenv("POD_NAME")
-	namespace := os.Getenv("NAMESPACE")
+	podName := EnvExists("POD_NAME", os.Getenv("POD_NAME"))
+	namespace := EnvExists("NAMESPACE", os.Getenv("NAMESPACE"))
+
 	//Set Logger
 	InitLogger(os.Stdout, os.Stderr)
 	// Inside k8s POD we need to initialise client with such function
@@ -104,33 +197,10 @@ func main() {
 		os.Exit(1)
 	}
 
-	command, deps := GetAnnotations(p.Annotations)
-
-	state := WAITING
-	if deps == nil {
-		state = READY
-	}
-
-	for state == WAITING {
-
-		for i := range deps {
-			service := strings.Trim(deps[i], " ")
-
-			CheckIfServiceExists(c, namespace, service)
-
-			if !CheckEndpointsAvailabilty(c, namespace, service) {
-				Info.Println(service, " service has no endpoints avaiable -> State waiting")
-				state = WAITING
-				break
-			}
-
-			Info.Println(service, " service has at least one running endpoint. -> State Ready")
-			state = READY
-
-		}
-
-	}
-	Info.Println("All dependencies resolved")
+	// renderConfigWithIP("/home/pprokop/Templates/test.conf")
+	command, serviceDeps, configs := GetAnnotations(p.Annotations)
+	WaitForServiceDependency(c, namespace, serviceDeps)
+	RenderConfigs(configs)
 	err = ExecuteCommandFromAnnotation(command)
 	if err != nil {
 		Error.Println(err)
